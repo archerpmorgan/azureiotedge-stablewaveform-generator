bcac2c2250c66fb08125164bbc1ace903d90b3a8
unknown <archerpmorgan@email.arizona.edu>
2018-06-05 14:40:18 -0700

    add specific desired properties for generating fnctns

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # Simulated Wave Form Generator for IoT Edge
 
 
+
+
+
+
+
+
+Below are the desired properties in Json used by the module.
+
+{
+    "properties": {
+        "desired": {
+            "SendData": true,
+            "Frequency": 1,
+            "Amplitude": 1,
+            "VerticalShift", 0,
+            "SendInterval": .05,
+            "WaveType", 1
+        }
+    }
+}
+
+SendData
+    starts or stops pushing messages to the output endpoint
+SendInterval
+    the amount of time in seconds between value publishes
+Frequency
+    the frequency of the wave in Hz
+Amplitude
+    the aplitude of the wave
+VerticalShift
+    shift the wave vertically by some constant amount
+WaveType
+    tells the simulator which type of wave to produce
+
+
--- WaveFormGeneratorModule/DesiredPropertiesData.cs
+++ WaveFormGeneratorModule/DesiredPropertiesData.cs
@@ -10,7 +10,15 @@ namespace WaveFormGeneratorModule
     public class DesiredPropertiesData
     {
         private bool _sendData = true;
-        private int _sendInterval = 5;
+        private double _sendInterval = .05;
+        private double _frequency = 1;
+        private double _amplitude = 1;
+        private double _verticalShift = 0;
+        private int _waveType = 1;
+
+
+
+
 
         public DesiredPropertiesData(TwinCollection twinCollection)
         {
@@ -25,6 +33,28 @@ namespace WaveFormGeneratorModule
                 if(twinCollection.Contains("SendInterval") && twinCollection["SendInterval"] != null)
                 {
                     _sendInterval = twinCollection["SendInterval"];
+                }
+
+                // begin custom desired properties for wave form module
+
+                if(twinCollection.Contains("Frequency") && twinCollection["Frequency"] != null)
+                {
+                    _frequency = twinCollection["Frequency"];
+                }
+
+                if(twinCollection.Contains("Amplitude") && twinCollection["Amplitude"] != null)
+                {
+                    _amplitude = twinCollection["Amplitude"];
+                }
+
+                if(twinCollection.Contains("VerticalShift") && twinCollection["VerticalShift"] != null)
+                {
+                    _verticalShift = twinCollection["VerticalShift"];
+                }
+
+                if(twinCollection.Contains("WaveType") && twinCollection["WaveType"] != null)
+                {
+                    _waveType = twinCollection["WaveType"];
                 }
             }
             catch(AggregateException aexc)
@@ -42,10 +72,19 @@ namespace WaveFormGeneratorModule
             {
                 Console.WriteLine($"Value for SendData = {_sendData}");
                 Console.WriteLine($"Value for SendInterval = {_sendInterval}");
+                Console.WriteLine($"Value for Frequency = {_frequency}");
+                Console.WriteLine($"Value for Amplitude = {_amplitude}");
+                Console.WriteLine($"Value for VerticalShift = {_verticalShift}");
+                Console.WriteLine($"Value for WaveType = {_waveType}");
+
             }
         }
 
         public bool SendData => _sendData;
-        public int SendInterval => _sendInterval;
+        public double SendInterval => _sendInterval;
+        public double Frequency => _frequency;
+        public double Amplitude => _amplitude;
+        public double VerticalShift => _verticalShift;
+        public int WaveType => _waveType;
     }
 }
